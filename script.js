7692bd7cb125082fc72fa4657510c38164bb989f
Jazmín <gomojazmin@gmail.com>
2025-02-11 00:30:31 -0600

    Añadí algunos ejemplos de tipos de datos en JS

---
+++ script.js
@@ -0,0 +1,54 @@
+/*
+TIPOS DE DATOS EN JAVASCRIPT
+*/
+
+// NUMBER
+var edad = 42;  
+console.log(edad);
+console.log(typeof(edad));
+
+
+// STRING
+var precio = 'Veinticinco';
+console.log(precio);
+console.log(typeof(precio));
+
+// NUMBER
+var puntos = -666;  
+console.log(puntos);
+console.log(typeof(puntos));
+
+// BOOLEAN
+var esMayorDeEdad = true;
+console.log(esMayorDeEdad);
+console.log(typeof(esMayorDeEdad));
+
+// NUMBER
+var articulosrestantes= 0;  
+console.log(articulosrestantes);
+console.log(typeof(articulosrestantes));
+
+// STRING
+var nombre = "Jazmin";
+console.log(nombre);
+console.log(typeof(nombre));
+
+// UNDEFINED
+var color;
+console.log(color);  
+console.log(typeof(color));          
+
+// NULL
+var apellido = null;
+console.log(apellido);
+console.log(typeof(apellido));
+
+// BOOLEAN
+var esVegetariano = false;
+console.log(esVegetariano);
+console.log(typeof(esVegetariano));
+
+// NaN
+var resultadoInvalido = 0 / hola;
+console.log(resultadoInvalido);
+console.log(typeof(resultadoInvalido));
